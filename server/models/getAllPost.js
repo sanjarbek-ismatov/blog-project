6412fe1c135265628bc7ef84c63d4472416cdd2a
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-08-12 22:02:21 +0500

    updated

--- server/models/getAllPost.js
+++ server/models/getAllPost.js
@@ -23,6 +23,7 @@ const postSchema = new mongoose.Schema({
     },
   },
 });
+// ok
 const Post = mongoose.model("post", postSchema);
 async function createPost({ title, content, image }, author) {
   const post = await new Post({
