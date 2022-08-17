208630a5c1d3a3c904b389044a9d7528ff98e080
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-08-17 21:18:55 +0500

    setting icon hover

--- client/pages/components/Navbar.js
+++ client/pages/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = ({ handleChange, value }) => {
         <svg
           onClick={() => router.push("/setting")}
           xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-settings"
+          className={style.icon}
           width="44"
           height="44"
           viewBox="0 0 24 24"
