dd1a1aeaf9102c0f477ab4769726b0773809a27c
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-08-18 15:20:31 +0500

    bugs

--- client/pages/post/[id]/index.js
+++ client/pages/post/[id]/index.js
@@ -11,39 +11,37 @@ const Post = ({ data }) => {
     sethydrate(true);
   }, []);
   if (!hydrate) return null;
-  return (
-    <div className={style.post}>
-      {data.map((e, i) => (
-        <>
-          <h1 className={style.h1}>{e.title}</h1>
-          <div key={i} className={style.postImage}>
-            <Image
-              className={style.image}
-              loader={() => e.image}
-              src={e.image}
-              alt="post-image"
-              width={1000}
-              height={550}
-              layout="responsive"
-            />
-          </div>
-          <div className={style.content}>
-            <p className={style.p}>{e.content}</p>
-          </div>
-          <div className={style.lowerContent}>
-            <p>{new Date(e.date).toLocaleTimeString()}</p>
-            <FontAwesomeIcon className={style.icon} icon={faHeart} />
-            <span
-              onClick={async () => await handleLike(e._id, e.likeCount)}
-              className={style.icon}
-            >
-              {e.likeCount}
-            </span>
-          </div>
-        </>
-      ))}
+  return data.map((e, i) => (
+    <div key={i} className={style.post}>
+      <h1 key={i} className={style.h1}>
+        {e.title}
+      </h1>
+      <div className={style.postImage}>
+        <Image
+          className={style.image}
+          loader={() => e.image}
+          src={e.image}
+          alt="post-image"
+          width={1000}
+          height={550}
+          layout="responsive"
+        />
+      </div>
+      <div className={style.content}>
+        <p className={style.p}>{e.content}</p>
+      </div>
+      <div className={style.lowerContent}>
+        <p>{new Date(e.date).toLocaleTimeString()}</p>
+        <FontAwesomeIcon className={style.icon} icon={faHeart} />
+        <span
+          onClick={async () => await handleLike(e._id, e.likeCount)}
+          className={style.icon}
+        >
+          {e.likeCount}
+        </span>
+      </div>
     </div>
-  );
+  ));
 };
 
 export default Post;
