0d0def9092bd1e2e9e1acf1b89ec9cbd3121a336
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-08-19 19:46:32 +0500

    main page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Nav, Button, NavigationBar, Main, H1, H2 } from "./components/styled";
 const Index = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <header>
+        <Nav>
+          <h1>My Blog</h1>
+          <NavigationBar>
+            <Button>Ro'yhatdan o'tish</Button>
+            <Button>Tizimga kirish</Button>
+          </NavigationBar>
+        </Nav>
+      </header>
+      <main>
+        <Main>
+          <div style={{ textAlign: "center" }}>
+            <H1>Assalomu alaykum</H1>
+            <hr />
+            <H2>My blog platformasiga xush kelibsiz!</H2>
+          </div>
+        </Main>
+      </main>
+    </div>
+  );
 };
 
 export default Index;
