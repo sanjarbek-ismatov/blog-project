450571e58209e060ed3d5e7c77da73b52ade6dbd
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2022-08-20 12:43:30 +0500

    testing seo

--- client/pages/index.js
+++ client/pages/index.js
@@ -15,14 +15,7 @@ const Index = () => {
       <Head>
         <title>MyBlog - bosh sahifa</title>
         <meta name="title" content="MyBlog - bosh sahifa" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="MyBlog - foydalanuvchilar tomonidan maqola yaratuvchi bepul va foydali platforma"
-        />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://my-blog-uz.vercel.app" />
         <meta property="og:title" content="MyBlog - bosh sahifa" />
         <meta
           property="og:description"
@@ -33,17 +26,6 @@ const Index = () => {
           content="https://media.istockphoto.com/photos/bloggingblog-concepts-ideas-with-worktable-picture-id922745190?k=20&m=922745190&s=612x612&w=0&h=TqsA7NeMPYXmK1TY5dsbdIsczaUK0OgguehHWdSUqL8="
         />
 
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://my-blog-uz.vercel.app" />
-        <meta property="twitter:title" content="MyBlog - bosh sahifa" />
-        <meta
-          property="twitter:description"
-          content="MyBlog - foydalanuvchilar tomonidan maqola yaratuvchi bepul va foydali platforma"
-        />
-        <meta
-          property="twitter:image"
-          content="https://media.istockphoto.com/photos/bloggingblog-concepts-ideas-with-worktable-picture-id922745190?k=20&m=922745190&s=612x612&w=0&h=TqsA7NeMPYXmK1TY5dsbdIsczaUK0OgguehHWdSUqL8="
-        />
         <link rel="shortcut icon" href="https://i.ibb.co/1XTN2WY/icon.png" />
       </Head>
       <header>
