121d5e7385aed3146c8567c6bec04d5f0b01c5b6
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-08-24 10:53:59 +0500

    Update [id].js

--- client/pages/post/[id].js
+++ client/pages/post/[id].js
@@ -2,9 +2,13 @@ import React from "react";
 import style from "../../styles/Post.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { NextSeo } from "next-seo";
+
 import NetworkMessage from "../../components/NetworkMessage";
 
+
+
+import NetworkMessage from "../../../components/NetworkMessage";
+import Axios from "axios";
 import Head from "next/head";
 const Post = ({ data, metadata }) => {
   const [hydrate, sethydrate] = useState(false);
@@ -14,30 +18,16 @@ const Post = ({ data, metadata }) => {
   if (!hydrate) return null;
   if (!data) return <NetworkMessage />;
   return (
-    <div>
-      <NextSeo
-        title={metadata.title}
-        description="My blog -  maqolalar sayti"
-        openGraph={{
-          type: "website",
-          title: metadata.title,
-          description: "My blog -  maqolalar sayti",
-          images: [
-            {
-              url: metadata.image,
-              width: "800",
-              height: "600",
-              alt: metadata.title,
-            },
-          ],
-        }}
-      />
+    <>
       <Head>
+        <title>{metadata.title}</title>
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content="My blog -  maqolalar sayti" />
+        <meta property="og:image" content={metadata.image} />
         <link rel="shortcut icon" href="https://i.ibb.co/1XTN2WY/icon.png" />
       </Head>
-
       {data.map((e, i) => (
-        <div smooth="true" key={i} className={style.post}>
+        <div key={i} className={style.post}>
           <h1 key={i} className={style.h1}>
             {e.title}
           </h1>
@@ -57,16 +47,19 @@ const Post = ({ data, metadata }) => {
           </div>
           <div className={style.lowerContent}>
             <p>{new Date(e.date).toLocaleTimeString()}</p>
+            {/* <FontAwesomeIcon className={style.icon} icon={faHeart} />
+        <span className={style.icon}>{e.likeCount}</span> */}
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
 export default Post;
-export async function getStaticProps({ params }) {
-  const { id } = params;
+export async function getServerSideProps({ params }) {
+  const id = params.id.replace(/ /g, "-");
+
   const res = await fetch(
     `https://blog-api-uz.herokuapp.com/api/get/post/`
   ).then((res) => res.json());
@@ -83,22 +76,5 @@ export async function getStaticProps({ params }) {
         image: data[0].image,
       },
     },
-  };
-}
-export async function getStaticPaths() {
-  const response = await fetch(
-    "https://blog-api-uz.herokuapp.com/api/get/post/"
-  );
-  const data = await response.json();
-  const paths = data.map((e, i) => {
-    return {
-      params: {
-        id: `${e.title.replace(/ /g, "-").toLowerCase()}`,
-      },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
   };
 }
