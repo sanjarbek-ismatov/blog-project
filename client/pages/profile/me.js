ba13bc95633c43dd508f2bcd32c40f73faf989dd
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-08-24 10:47:23 +0500

    Update me.js

--- client/pages/profile/me.js
+++ client/pages/profile/me.js
@@ -1,51 +1,4 @@
-// import React from "react";
 
-// const me = ({ user }) => {
-//   if (typeof window !== "undefined") {
-//     console.log(user, localStorage.token);
-//   }
-//   return <div></div>;
-// };
-
-// export default me;
-// export async function getServerSideProps() {
-//   if (typeof window !== "undefined") {
-//     const data = await fetch(
-//       "https://blog-api-uz.herokuapp.com/api/profile/me",
-//       {
-//         method: "GET",
-//         headers: {
-//           ["x-token"]: localStorage.token,
-//         },
-//       }
-//     )
-//       .then((res) => res.json())
-//       .then((data) => console.log(data));
-//     return {
-//       props: {
-//         user: data,
-//       },
-//     };
-//   }
-//   return {
-//     props: {
-//       user: "",
-//     },
-//   };
-// }
-// // const data = await axios.get(
-// //     "https://blog-api-uz.herokuapp.com/api/profile/me",
-// //     {
-// //       headers: {
-// //         ["x-token"]: localStorage.getItem("token"),
-// //       },
-// //     }
-// //   );
-// //   return {
-// //     props: {
-// //       user: data,
-// //     },
-// //   };
 import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect } from "react";
 import { getProfileMeThunk } from "../../state/thunks/getProfileMeThunk";
@@ -109,7 +62,7 @@ const Me = () => {
             <ul className={style.ul}>
               {user.posts.map((e, i) => {
                 return (
-                  <li>
+                  <li key={i}>
                     <Link
                       href={`/post/${e.title.toLowerCase().replace(/ /g, "-")}`}
                     >
