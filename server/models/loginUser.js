859d010ddd7708fde10c2d9072b5ff37507227ee
Sanjarbek <ismatovsanjarbek@yandex.ru>
2022-08-30 18:04:56 +0500

    validate fix

--- server/models/loginUser.js
+++ server/models/loginUser.js
@@ -4,9 +4,10 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 async function loginUser(body) {
   const user = await User.findOne({ email: body.email });
+  if (!user) return;
   const passwordCheck = await bcrypt.compare(body.password, user.password);
 
-  if (!user || !passwordCheck) return;
+  if (!passwordCheck) return;
   return jwt.sign({ _id: user._id }, "VERYSTRONGKEY");
 }
 module.exports = loginUser;
